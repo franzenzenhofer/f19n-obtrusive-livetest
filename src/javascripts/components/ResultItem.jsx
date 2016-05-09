6fc759b0b4e0707e7049c1a2940a1be9fe90b07d
Moritz Kobrna <github@kobrna.net>
2016-05-09 14:55:27 +0200

    view type beside the label

--- src/javascripts/components/ResultItem.jsx
+++ src/javascripts/components/ResultItem.jsx
@@ -7,7 +7,7 @@ export default function ResultItem(props) {
   return (
     <div className={classNames(type, label.replace(/[^A-Z0-9]/i, '').toLowerCase(), 'result')}>
       <div className="meta">
-        <span className="label">{label}</span>
+        <span className="label">{label} ({type})</span>
       </div>
       <div className="message" dangerouslySetInnerHTML={{ __html: message }} />
     </div>
