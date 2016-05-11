1fc6090b19952c2c0e0c1c36be3df082be565308
Moritz Kobrna <github@kobrna.net>
2016-05-11 02:20:38 +0200

    typo fix

--- src/javascripts/panel.js
+++ src/javascripts/panel.js
@@ -18,7 +18,7 @@ chrome.runtime.sendMessage('tabIdPls', (response) => {
   tabId = response.tabId;
   const storeKey = resultStoreKey(tabId);
   chrome.storage.local.get(storeKey, (data) => {
-    const results = data[storeKey];
-    ReactDOM.render(<ResultList onClosePanelRequest={handleClosePanelRequest} oreKey={storeKey} results={results || []} />, document.getElementById('app'));
+    const results = data[storeKey] || [];
+    ReactDOM.render(<ResultList onClosePanelRequest={handleClosePanelRequest} storeKey={storeKey} results={results} />, document.getElementById('app'));
   });
 });
