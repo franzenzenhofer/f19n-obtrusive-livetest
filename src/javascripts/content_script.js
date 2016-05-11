e8262cddb9319de22a18a8ba7327a4ec6ec11e33
Moritz Kobrna <github@kobrna.net>
2016-05-11 12:50:15 +0200

    cleanup changed positions when hide panel

--- src/javascripts/content_script.js
+++ src/javascripts/content_script.js
@@ -10,16 +10,13 @@ const fixAbsolutePositionedElements = () => {
   const wrapperWidth = $panelWrapper.width();
   $('*').each((index, element) => {
     if (!$(element).hasClass('f19n-panel-wrapper') && $(element).css('position') === 'fixed' && $(element).css('left') === '0px') {
-      $(element).data('f19n-fix-original-left', $(element).css('left')).css({ left: wrapperWidth });
+      $(element).data('f19n-fixed-element', true).css({ left: wrapperWidth });
     }
   });
 };
 
 const unfixAbsolutePositionedElements = () => {
-  $('[data=f19n-fix-original-left]').each((index, element) => {
-    const originalLeft = $(element).data('f19n-fix-original-left');
-    $(element).removeData('f19n-fix-original-left').css({ left: originalLeft });
-  });
+  $('[data-f19n-fixed-element]').css({ left: 0 });
 };
 
 const showPanel = () => {
