2ed4b085f92d52d63912434c7ba0f03cc5ccd7df
Franz Enzenhofer <f.enzenhofer@gmail.com>
2016-05-23 12:21:25 +0200

    Class to classname

--- src/javascripts/components/Panel/ResultItem.jsx
+++ src/javascripts/components/Panel/ResultItem.jsx
@@ -6,11 +6,11 @@ export default function ResultItem(props) {
 
   return (
     <tr className={classNames(type, label.replace(/[^A-Z0-9]/i, '').toLowerCase(), 'result')}>
-      <td class="type">{type}</td>
-      <td class="meta label">
+      <td className="type">{type}</td>
+      <td className="meta label">
         <a href="#" className="filter filter-by-label" onClick={props.onLabelClick}>{label}</a>
       </td>
-      <td class="message"><span dangerouslySetInnerHTML={{ __html: message }} /></td>
+      <td className="message"><span dangerouslySetInnerHTML={{ __html: message }} /></td>
     </tr>
   );
 }
