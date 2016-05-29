66700662fb601ff412ab82bf35389a03961e0131
Franz Enzenhofer <f.enzenhofer@gmail.com>
2016-05-29 19:29:51 +0200

    rel alternate

--- sample-rules/statusCode.js
+++ sample-rules/statusCode.js
@@ -2,12 +2,15 @@ function(page) {
   var sc = page.getStatusCode();
   var url = page.getURL();
   if (sc) {
-    var text = `${url} → <a href="https://en.wikipedia.org/wiki/List_of_HTTP_status_codes" target="_top">HTTP ${sc}</a>`;
-    var label = 'info';
-    if (sc < 200) { label = 'warning'; }
-    if (sc >= 300) { label = 'warning'; }
-    if (sc >= 400) { label = 'error'; }
-    return this.createResult(1, 'HTTP', text, label);
+    var type = 'info';
+    var anchor = '';
+    if (sc < 200) { type = 'warning'; anchor='#1xx_Informational';}
+    if (sc <= 200) { anchor='#2xx_Success';}
+    if (sc >= 300) { type = 'warning'; anchor='#3xx_Redirection'; }
+    if (sc >= 400) { type = 'error'; anchor="#4xx_Client_Error";}
+    if (sc >= 500) { type ='error'; anchor="#5xx_Server_Error" }
+    var text = `${url} → <a href="https://en.wikipedia.org/wiki/List_of_HTTP_status_codes${anchor}" target="_top">HTTP ${sc}</a>`;
+    return this.createResult(1, 'HTTP', text, type);
   }
   return null;
 }
