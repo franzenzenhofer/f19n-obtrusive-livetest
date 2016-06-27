b7b423198a249fd12106484f8aa0b64299490e8f
Moritz Kobrna <github@kobrna.net>
2016-06-27 18:10:06 +0200

    remove edit, delete and download for default rules

--- src/javascripts/components/Rules/RuleListItem.js
+++ src/javascripts/components/Rules/RuleListItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classNames from 'classnames';
 
 export default function RulesListItem(props) {
-  const { name, id, status, onEditClick, onDeleteClick, onStatusClick, onDuplicateClick, index } = props;
+  const { name, id, status, onEditClick, onDeleteClick, onStatusClick, onDuplicateClick, index, defaultRule } = props;
 
   function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -13,14 +13,14 @@ export default function RulesListItem(props) {
       <td className="RuleTable-ruleName">
         <a onClick={onEditClick}><h3>{name}</h3><span>#{capitalizeFirstLetter(id)} / Index: {index}</span></a>
         <div>
-          <button className="Button Button--haptic" onClick={onEditClick}>Edit</button>
+          {!defaultRule && <button className="Button Button--haptic" onClick={onEditClick}>Edit</button>}
           <button className="Button Button--haptic" onClick={onDuplicateClick}>Duplicate</button>
-          <button className="Button Button--haptic">Download</button>
+          {!defaultRule && <button className="Button Button--haptic">Download</button>}
         </div>
       </td>
       <td className="RuleTable-ruleActions">
         <button className="Button Button--haptic" onClick={onStatusClick}>{status === 'enabled' ? 'disable' : 'enable'}</button>
-        <button className="Button Button--haptic" onClick={onDeleteClick}>Delete</button>
+        {!defaultRule && <button className="Button Button--haptic" onClick={onDeleteClick}>Delete</button>}
       </td>
     </tr>
   );
