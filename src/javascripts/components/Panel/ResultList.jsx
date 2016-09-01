5fb4aba1580869054d13588d2e84b43e1ca955ff
Moritz Kobrna <github@kobrna.net>
2016-09-01 15:03:28 +0200

    clean scope for the whole panel via iframe wrapper

--- src/javascripts/components/Panel/ResultList.jsx
+++ src/javascripts/components/Panel/ResultList.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import ResultItem from './ResultItem';
 
-import { sortBy } from 'lodash';
-
 export default class ResultList extends Component {
   constructor(props) {
     super(props);
@@ -16,7 +14,6 @@ export default class ResultList extends Component {
     chrome.storage.onChanged.addListener(onStoreChange);
 
     this.closePanelClick = this.closePanelClick.bind(this);
-    this.clearFilter = this.clearFilter.bind(this);
   }
 
   onStoreChange(data) {
@@ -30,16 +27,17 @@ export default class ResultList extends Component {
     this.setState({ filter: label });
   }
 
-  clearFilter() {
+  clearFilter = () => {
     this.setState({ filter: null });
   }
 
   resultItems(resultData, index) {
-    return <ResultItem {...resultData} onLabelClick={() => this.setFilter(resultData.label) } onTypeClick={() => this.setFilter(resultData.type) } key={`result-${index}`} />;
+    return <ResultItem {...resultData} onLabelClick={() => this.setFilter(resultData.label) } onTypeClick={() => this.setFilter(resultData.type)} key={`result-${index}`} />;
   }
 
-  closePanelClick() {
+  closePanelClick(e) {
     this.props.onClosePanelRequest();
+    e.preventDefault();
   }
 
   render() {
@@ -52,12 +50,21 @@ export default class ResultList extends Component {
     const clearFilterLink = this.state.filter ? <a href="javascript://" className="clear-filter" onClick={this.clearFilter}>Clear filter</a> : null;
 
     setTimeout(() => {
-      window.parent.postMessage({ height: document.querySelector('#app').offsetHeight }, '*');
-    }, 100);
+      window.parent.postMessage({ rendered: true }, '*');
+    }, 10);
 
     return (
       <div className="f19n-panel">
         <div className="Header">
+          <div className="Header-brand u-cf">
+            <svg className="ic u-floatLeft" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#6E772F" d="M11.7 17.2L2 15l10.2-2.4L22 15"/><path fill="#4F5429" d="M11.7 21.7L2 19.5V15l9.7 2.2"/><path fill="#8E9A34" d="M22 19.5l-10.3 2.2v-4.5L22 15"/><g><path fill="#6E772F" d="M11.7 12L2 9.7l10.2-2.3L22 9.8"/><path fill="#4F5429" d="M11.7 16.5L2 14.2V9.7l9.7 2.3"/><path fill="#8E9A34" d="M22 14.3l-10.3 2.2V12L22 9.8"/></g><g><path fill="#6E772F" d="M11.7 6.9L2 4.6l10.2-2.3L22 4.7"/><path fill="#4F5429" d="M11.7 11.4L2 9.1V4.6l9.7 2.3"/><path fill="#8E9A34" d="M22 9.2l-10.3 2.2V6.9L22 4.7"/></g></svg>
+            <h2 className="u-floatLeft">Obtrusive Live Test</h2>
+            <a className="u-floatRight" onClick={this.closePanelClick} href="#">
+              <svg className="ic ic-close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/><path d="M0 0h24v24H0z" fill="none"/>
+              </svg>
+            </a>
+          </div>
           <div className="Header-controls u-cf">
             <a className="Button u-floatLeft" target="_blank" href={chrome.extension.getURL('rules.html')}>Manage rules</a>
             <span className="u-floatRight">{clearFilterLink}</span>
--- src/javascripts/content_script.js
+++ src/javascripts/content_script.js
@@ -1,28 +1,44 @@
 /* global $ */
+import Frame from 'react-frame-component';
 import Draggable from 'draggable';
 
-const header = `
-  <div class="Header">
-    <div class="Header-brand u-cf">
-      <svg class="ic u-floatLeft" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#6E772F" d="M11.7 17.2L2 15l10.2-2.4L22 15"/><path fill="#4F5429" d="M11.7 21.7L2 19.5V15l9.7 2.2"/><path fill="#8E9A34" d="M22 19.5l-10.3 2.2v-4.5L22 15"/><g><path fill="#6E772F" d="M11.7 12L2 9.7l10.2-2.3L22 9.8"/><path fill="#4F5429" d="M11.7 16.5L2 14.2V9.7l9.7 2.3"/><path fill="#8E9A34" d="M22 14.3l-10.3 2.2V12L22 9.8"/></g><g><path fill="#6E772F" d="M11.7 6.9L2 4.6l10.2-2.3L22 4.7"/><path fill="#4F5429" d="M11.7 11.4L2 9.1V4.6l9.7 2.3"/><path fill="#8E9A34" d="M22 9.2l-10.3 2.2V6.9L22 4.7"/></g></svg>
-      <h2 class="u-floatLeft">Loading …</h2>
-      <a class="u-floatRight close-panel" href="#">
-        <svg class="ic ic-close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/><path d="M0 0h24v24H0z" fill="none"/>
-        </svg>
-      </a>
-    </div>
-  </div>
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Panel from './components/Panel/Panel';
+
+import resultStoreKey from './utils/resultStoreKey';
+
+const FRAME_TEMPLATE = `
+  <html>
+    <head>
+      <link href="${chrome.extension.getURL('css/normalize.css')}" rel="stylesheet" type="text/css" />
+      <link href="${chrome.extension.getURL('css/panel.css')}" rel="stylesheet" type="text/css" />
+      <base target="_top" />
+    <body>
+      <div id="panel"></div>
+    </body>
+  </html>
 `;
 
-const panelUrl = chrome.extension.getURL('panel.html');
-const $panelWrapper = $(`<div class="f19n-panel-wrapper">${header}<iframe src='${panelUrl}'></iframe></div>`);
-const $iframe = $panelWrapper.find('iframe');
-
-$iframe.on('load', () => {
-  $panelWrapper.find('h2').text('Obtrusive Live Test');
-  $(window).trigger('resize');
+const $appRoot = $('<style>#f19n-panel-root:before { content: " "; cursor: move; position: absolute; left: 0; top: 0; width: calc(100% - 20px); height: 20px; background: transparent;}</style><div id="f19n-panel-root"></div>').css({
+  position: 'fixed',
+  top: '10px',
+  left: '10px',
+  width: '350px',
+  zIndex: 99999,
+  backgroundColor: 'white',
+  boxShadow: '0px 0px 8px rgba(0,0,0,0.2)',
+  maxHeight: '300px',
 });
+
+const check = (sites, url) => {
+  const entry = sites.reverse().find((l) => {
+    const regexp = `\^${l}\$`.replace(/\*/g, '[^ ]*').replace('!', '');
+    return url.match(new RegExp(regexp));
+  });
+  return entry ? (entry.match(/^!.+/) !== null ? false : true) : false;
+};
 
 const getTab = (callback) => {
   chrome.runtime.sendMessage('tabIdPls', (response) => {
@@ -39,14 +55,6 @@ const handleClosePanelRequest = () => {
   });
 };
 
-const check = (sites, url) => {
-  const entry = sites.reverse().find((l) => {
-    const regexp = `\^${l}\$`.replace(/\*/g, '[^ ]*').replace('!', '');
-    return url.match(new RegExp(regexp));
-  });
-  return entry ? (entry.match(/^!.+/) !== null ? false : true) : false;
-};
-
 const panelShouldBeVisible = (data, tabId) => {
   const hiddenPanels = data['hidden-panels'] || [];
   const enabledSites = data.sites;
@@ -56,26 +64,37 @@ const panelShouldBeVisible = (data, tabId) => {
   return !hidden;
 };
 
+const getAppRootElement = () => {
+  return document.getElementById('f19n-panel-root');
+};
+
 const getPanelPosition = (data, host) => {
   return data[`panel-position-${host}`] || [10, 10];
 };
 
-const showPanel = () => {
-  if (!$('.f19n-panel-wrapper').length) {
-    $('body').append($panelWrapper);
+const showPanel = (url, tabId, onMount) => {
+  if (!$('#f19n-panel-root').length) {
+    $('body').append($appRoot.clone());
+
+    const storeKey = resultStoreKey(tabId);
+    chrome.storage.local.get(storeKey, (data) => {
+      const results = data[storeKey] || [];
+      const node = document.getElementById('f19n-panel-root');
+      ReactDOM.render(<Frame contentDidMount={onMount} style={{ border: 'none', width: '100%', height: '100%' }} initialContent={FRAME_TEMPLATE} mountTarget="#panel"><Panel onClosePanelRequest={handleClosePanelRequest} storeKey={storeKey} results={results} url={url} /></Frame>, node);
+    });
   }
-  $panelWrapper.find('a.close-panel').on('click', handleClosePanelRequest);
+  $(getAppRootElement()).find('a.close-panel').on('click', handleClosePanelRequest);
 };
 
 const hidePanel = () => {
-  $panelWrapper.remove();
-  $panelWrapper.find('a.close-panel').off('click', handleClosePanelRequest);
+  $(getAppRootElement()).find('a.close-panel').off('click', handleClosePanelRequest);
+  $(getAppRootElement()).remove();
 };
 
 const getLimitBoundings = () => {
   return {
-    x: [10, window.innerWidth - 10 - $panelWrapper.width()],
-    y: [10, window.innerHeight - 10 - $panelWrapper.height()],
+    x: [10, window.innerWidth - 10 - $(getAppRootElement()).width()],
+    y: [10, window.innerHeight - 10 - $(getAppRootElement()).height()],
   };
 };
 
@@ -86,8 +105,10 @@ const initializePanel = ({ position, host }) => {
   };
 
   const snapPanel = (draggablePanel) => {
-    const panelOffset = $panelWrapper.offset();
+    const panelOffset = $(getAppRootElement()).offset();
     const limit = getLimitBoundings();
+
+    console.log(panelOffset);
 
     const { x, y } = draggablePanel.get();
 
@@ -102,7 +123,7 @@ const initializePanel = ({ position, host }) => {
     }
   };
 
-  const draggablePanel = new Draggable($panelWrapper.get(0), {
+  const draggablePanel = new Draggable(getAppRootElement(), {
     setPosition: false,
     onDragEnd: (element, x, y) => {
       chrome.storage.local.set({ [`panel-position-${host}`]: [x, y] });
@@ -112,9 +133,9 @@ const initializePanel = ({ position, host }) => {
   draggablePanel.set(position[0], position[1]);
   setLimit(draggablePanel);
 
-  window.addEventListener('message', (result) => {
-    const { height } = result.data;
-    $iframe.css({ height: Math.min(window.innerHeight - 20, height) });
+  window.addEventListener('message', () => {
+    const height = $(getAppRootElement()).find('iframe').get(0).contentDocument.querySelector('#panel').offsetHeight;
+    $(getAppRootElement()).css({ height: Math.min(window.innerHeight - 20, height) });
     setLimit(draggablePanel);
     snapPanel(draggablePanel);
   });
@@ -125,26 +146,30 @@ const initializePanel = ({ position, host }) => {
   });
 };
 
-chrome.storage.local.get((data) => {
-  getTab(({ url, tabId }) => {
-    const host = (new URL(url)).host;
-    const visible = panelShouldBeVisible(data, tabId);
-    const position = getPanelPosition(data, host);
-    initializePanel({ position, host });
-    if (visible) { showPanel(); }
+const init = () => {
+  chrome.storage.local.get((data) => {
+    getTab(({ url, tabId }) => {
+      const host = (new URL(url)).host;
+      const visible = panelShouldBeVisible(data, tabId);
+      const position = getPanelPosition(data, host);
+      const onMount = () => {
+        initializePanel({ position, host });
+      };
+      if (visible) {
+        showPanel(url, tabId, onMount);
+      } else {
+        hidePanel();
+      }
+    });
   });
-});
+};
 
 chrome.storage.onChanged.addListener((data) => {
-  getTab(({ tabId }) => {
+  getTab(() => {
     if (Object.keys(data)[0] === 'hidden-panels') {
-      const hiddenPanels = data['hidden-panels'].newValue || [];
-      const hidden = hiddenPanels.indexOf(tabId) !== -1;
-      if (hidden) {
-        hidePanel();
-      } else {
-        showPanel();
-      }
+      init();
     }
   });
 });
+
+init();
