eb8ef7acd96c92f92733786f42e40e713c0d0f28
hg-lr <holger.guggi@gmail.com>
2016-10-10 12:06:52 +0200

    footer changes

--- src/javascripts/components/Panel/Footer.jsx
+++ src/javascripts/components/Panel/Footer.jsx
@@ -8,12 +8,12 @@ export default function Panel(props) {
   const {url} = props;
   return (
     <footer>
-      <a href={`${url}`} target="_top" title="Reload"><button class="Button Button--haptic Button--inline">↻</button></a>
-      <a href={`https://developers.google.com/speed/pagespeed/insights/?hl=en&url=${url}`} target="_blank" title="Page Speed Insights"><button>Psi</button></a>
-      <a href={`https://www.webpagetest.org?url=${url}`} target="_blank" title="WebPageTest.org"><button>Wpt</button></a>
-      <a href={`https://www.google.com/webmasters/tools/mobile-friendly/?hl=en&url=${url}`} target="_blank" title="Mobile Friendly Test"><button>Mf</button></a>
-      <a href={`https://search.google.com/structured-data/testing-tool?hl=en&url=${url}`} target="_blank" title="Structured Data Testing Tool"><button>Sd</button></a>
-      <a href={`https://developers.facebook.com/tools/debug/sharing/?q=${url}`} target="_blank" title="Facebook Debugger"><button>Fbd</button></a>
+      <a href={`${url}`} target="_top" title="Reload"><button className="Button Button--haptic">↻</button></a>
+      <a href={`https://developers.google.com/speed/pagespeed/insights/?hl=en&url=${url}`} target="_blank" title="Page Speed Insights"><button className="Button Button--haptic">Psi</button></a>
+      <a href={`https://www.webpagetest.org?url=${url}`} target="_blank" title="WebPageTest.org"><button className="Button Button--haptic">Wpt</button></a>
+      <a href={`https://www.google.com/webmasters/tools/mobile-friendly/?hl=en&url=${url}`} target="_blank" title="Mobile Friendly Test"><button className="Button Button--haptic">Mf</button></a>
+      <a href={`https://search.google.com/structured-data/testing-tool?hl=en&url=${url}`} target="_blank" title="Structured Data Testing Tool"><button className="Button Button--haptic">Sd</button></a>
+      <a href={`https://developers.facebook.com/tools/debug/sharing/?q=${url}`} target="_blank" title="Facebook Debugger"><button className="Button Button--haptic">Fbd</button></a>
     </footer>
   );
 }
