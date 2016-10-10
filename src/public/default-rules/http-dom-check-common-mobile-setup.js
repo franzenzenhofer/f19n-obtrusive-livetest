c04920e3fe8f4e23001bd32720f3e99a90411fdd
Franz Enzenhofer <f.enzenhofer@gmail.com>
2016-10-10 17:59:24 +0200

    prettier & check for mobile rule

---
+++ src/public/default-rules/http-dom-check-common-mobile-setup.js
@@ -0,0 +1,19 @@
+function(page){
+  var hh = page.getHttpHeaders("last");
+  var vary = hh['vary'] || "";
+  var isvary = function(){if(vary.indexOf("agent")===-1){ return false} return true}();
+  var u = page.getURL("last");
+  var idle_dom = page.getIdleDom();
+  var medias = idle_dom.querySelectorAll("link[rel=alternate][media][href]");
+  var meta_viewports = idle_dom.querySelectorAll('meta[name=viewport]');
+  console.log('common mobile check');
+  console.log(isvary);
+  console.log(medias && medias.length>0);
+  console.log(meta_viewports && meta_viewports.length > 0);
+
+  if(!(isvary || (medias && medias.length>0) || (meta_viewports && meta_viewports.length > 0)))
+  {
+    return this.createResult('HTTP DOM', "No common mobile setup (<a href='https://developers.google.com/webmasters/mobile-sites/mobile-seo/' target='_blank'>responsive, dynamic serving, different URL</a>) discovered! <a href='https://www.google.com/webmasters/tools/mobile-friendly/?hl=en&url="+u+"' target='_blank'>Mobile Friendly Test</a>", "error");
+  }
+  return null;
+}
---
+++ src/public/default-rules/idle-dom-rel-alternate-media.js
@@ -0,0 +1,16 @@
+function(page) {
+  var idle_dom = page.getIdleDom();
+  var medias = idle_dom.querySelectorAll("link[rel=alternate][media][href]");
+  if(medias && medias.length > 0)
+  {
+    var murl = "";
+    var more = "("+medias.length+")";
+    if (medias.length===1)
+    {
+      murl = medias[0].href.trim();
+      more = "<a href='"+murl+"' target='_blank'>"+murl+"</a>"
+    }
+    return this.createResult("DOM","Seperate mobile URL "+more+" discovered."+this.partialCodeLink(medias), "info", "Idle");
+  }
+  return null;
+}
