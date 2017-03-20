6b30334f5c4d473213cee34ef7436bdda5624c1e
Moritz Kobrna <github@kobrna.net>
2017-03-20 14:43:57 +0100

    #49 remove unsupported default rules

--- src/javascripts/utils/syncDefaultRules.js
+++ src/javascripts/utils/syncDefaultRules.js
@@ -1,15 +1,38 @@
+import { difference } from 'lodash';
 import rulesStore from './../store/rules';
 
-const updateOrImportRules = (rulesToImport) => {
+const updateOrImportRules = (rulesToImport, done = () => {}) => {
   const { name, body } = rulesToImport.shift();
   rulesStore.all((rules) => {
     const existingRuleIndex = rules.findIndex(r => r.get('name') === name && r.get('defaultRule') === true);
-    const cb = rulesToImport.length > 0 ? () => { updateOrImportRules(rulesToImport); } : null;
+    const cb = rulesToImport.length > 0 ? () => { updateOrImportRules(rulesToImport, done); } : done;
     if (existingRuleIndex !== -1) {
       rulesStore.update(existingRuleIndex, { body }, cb);
     } else {
       rulesStore.add({ name, body, defaultRule: true }, cb);
     }
+  }, true);
+};
+
+const deleteUnsupportedRules = (supportedRules) => {
+  const deleteRules = (rules = []) => {
+    const ruleToDelete = rules.shift();
+    rulesStore.remove((r) => {
+      return r.get('name') === ruleToDelete;
+    }, () => {
+      if (rules.length > 0) {
+        deleteRules(rules);
+      }
+    });
+  };
+
+  rulesStore.all((allRules) => {
+    const allRulesNames = allRules.toJS().map(r => r.name);
+    const supportedRulesNames = supportedRules.map(r => r.name);
+
+    const ruleNamesToDelete = difference(allRulesNames, supportedRulesNames);
+
+    deleteRules(ruleNamesToDelete);
   }, true);
 };
 
@@ -31,7 +54,9 @@ export default (path = 'default-rules') => {
               rulesList.push({ name, body });
 
               if (rulesList.length >= jsFilesOnly.length) {
-                updateOrImportRules(rulesList);
+                updateOrImportRules(rulesList.slice(), () => {
+                  deleteUnsupportedRules(rulesList);
+                });
               }
             };
             fileReader.readAsText(file);
