7491d5048f3c436631cf9aa8d0ad51e10bdae510
Franz Enzenhofer <f.enzenhofer@gmail.com>
2017-04-25 12:05:44 +0200

    "done" refactor

--- src/public/default-rules/static-head-title.js
+++ src/public/default-rules/static-head-title.js
@@ -1,4 +1,4 @@
-function(page) {
+function(page, done) {
   var dom = page.getStaticDom();
   const what = 'static';
   var location = page.getLocation('static');
@@ -11,19 +11,19 @@ function(page) {
   if (titletags.length > 0) {
     if(titletags.length === 1)
     {
-    return this.createResult(lable, 'Title: '+titletags[0].innerText+this.partialCodeLink(titletags), 'info', what);
+    done(this.createResult(lable, 'Title: '+titletags[0].innerText+this.partialCodeLink(titletags), 'info', what));
     //check size //throw to short, throw to long
     //check brand
     //TODO check for common non descriptive titles
     }
     else
     {
-    return this.createResult(lable, '<a href="https://support.google.com/webmasters/answer/35624?hl=en#3">Multiple title-tags found.</a> <a href="https://www.w3.org/Provider/Style/TITLE.html">There should be only one.</a>'+this.partialCodeLink(titletags), 'error', what);
+    done(this.createResult(lable, '<a href="https://support.google.com/webmasters/answer/35624?hl=en#3">Multiple title-tags found.</a> <a href="https://www.w3.org/Provider/Style/TITLE.html">There should be only one.</a>'+this.partialCodeLink(titletags), 'error', what));
     }
   }
   else
   {
-    return this.createResult(lable, '<a href="https://support.google.com/webmasters/answer/35624?hl=en#3">No title-tag found.</a>', 'error', what);
+    done(this.createResult(lable, '<a href="https://support.google.com/webmasters/answer/35624?hl=en#3">No title-tag found.</a>', 'error', what));
   }
   return null;
 }
