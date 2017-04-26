e16ddd6ead2b9925cd1f2405a182f3ffcea02d5c
Franz Enzenhofer <f.enzenhofer@gmail.com>
2017-04-26 15:00:51 +0200

    Blank done()

--- src/public/default-rules/static-head-amp.js
+++ src/public/default-rules/static-head-amp.js
@@ -7,4 +7,5 @@ function(page, done) {
     var text = `Amp: <a href='${href}' target='_top'>${href}</a> ${this.partialCodeLink(amp)} <a href="https://validator.ampproject.org/#url=${href}">Validate</a>`;
     done(this.createResult('HEAD', text, 'info', 'static'));
   }
+  done();
 }
