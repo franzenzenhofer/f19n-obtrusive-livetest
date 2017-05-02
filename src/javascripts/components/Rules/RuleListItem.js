543782c52d97e139a84fd90d5359becfbbf0757c
Moritz Kobrna <github@kobrna.net>
2017-05-02 15:59:32 +0200

    Mark unconfigured configurable rules in list view

--- src/javascripts/components/Rules/RuleListItem.js
+++ src/javascripts/components/Rules/RuleListItem.js
@@ -4,7 +4,7 @@ import classNames from 'classnames';
 import { configurableKeyMatcher } from './../../utils/configurableRules';
 
 export default function RulesListItem(props) {
-  const { name, body, id, status, onViewClick, onDeleteClick, onStatusClick, index, defaultRule } = props;
+  const { name, body, id, status, onViewClick, onDeleteClick, onStatusClick, index, defaultRule, configuration } = props;
 
   function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -12,10 +12,13 @@ export default function RulesListItem(props) {
 
   const configurable = body.match(configurableKeyMatcher());
 
+  const configured = configurable && Object.values(configuration || {}).filter(a => a.length).length === configurable.length;
+  const configurationHTML = configured ? <span className="rule-configurable">Configurable</span> : <span className="rule-missing-configuration">Missing configuration</span>;
+
   return (
     <tr className={classNames('rule', { disabled: status === 'disabled' })}>
       <td className="RuleTable-ruleName">
-        <a onClick={onViewClick}><h3>{name} {configurable ? '(configurable)' : null}</h3><span>#{capitalizeFirstLetter(id)} / Index: {index}</span></a>
+        <a onClick={onViewClick}><h3>{name} {configurable && configurationHTML}</h3><span className="rule-meta">#{capitalizeFirstLetter(id)} / Index: {index}</span></a>
         <div>
           {<button className="Button Button--haptic" onClick={onViewClick}>View</button>}
         </div>
