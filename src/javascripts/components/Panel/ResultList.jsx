afea2f6eb2c04a611ba2e6590d34e4606af8e952
hg-lr <holger.guggi@gmail.com>
2017-08-07 17:41:12 +0200

    settings

--- src/javascripts/components/Panel/ResultList.jsx
+++ src/javascripts/components/Panel/ResultList.jsx
@@ -71,7 +71,7 @@ export default class ResultList extends Component {
             </a>
           </div>
           <div className="Header-controls u-cf">
-            <a className="Button u-floatLeft" target="_blank" href={chrome.extension.getURL('rules.html')}>Settings</a>
+            <a className="Button u-floatLeft" target="_blank" href={chrome.extension.getURL('rules.html')}>&#9881 Settings</a>
             <span className="u-floatRight">{clearFilterLink}</span>
           </div>
         </div>
