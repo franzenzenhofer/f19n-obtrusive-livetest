76a8f571ca2dcebb98fbee7f61706d2622ccc3fc
Franz Enzenhofer <f.enzenhofer@gmail.com>
2017-08-08 12:23:21 +0200

    New push

--- src/javascripts/content_script.js
+++ src/javascripts/content_script.js
@@ -21,6 +21,7 @@ const FRAME_TEMPLATE = `
   </html>
 `;
 
+
 //this  defines the area where the dragevent can start
 const $appRoot = $('<style>#f19n-panel-root:before { content: " "; cursor: move; position: absolute; left: 0; top: 0; width: calc(100% - 90px); height: 20px; background: transparent;}</style><div id="f19n-panel-root"></div>').css({
   position: 'fixed',
