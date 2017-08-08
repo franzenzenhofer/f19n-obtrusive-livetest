f2e5bfce2b6e16859e6afcebf56f0ce8320c0855
Franz Enzenhofer <f.enzenhofer@gmail.com>
2017-08-08 12:19:01 +0200

    Wording change

--- src/javascripts/components/Rules/EnabledSites.js
+++ src/javascripts/components/Rules/EnabledSites.js
@@ -5,7 +5,7 @@ export default class EnabledSites extends Component {
     return (
       <div className="Header-subHeader Section sites">
         <div className="Wrapper">
-          <h2>Enabled sites</h2>
+          <h2>Enabled sites - Whitelist / Blacklist</h2><div>Blacklisted URL-schemas start with !</div>
           <textarea style={{ height: 50, width: '100%' }} defaultValue={this.props.sites} onChange={(e) => this.props.onChange(e.target.value)} />
           <p>
             <button className="Button Button--haptic" onclick={(e) => this.props.onChange(e.target.value)}>Apply</button>
