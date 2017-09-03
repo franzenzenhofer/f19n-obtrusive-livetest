96e67d4e98bce0b63e92bde9e1b789599cef6bcb
Franz Enzenhofer <f.enzenhofer@gmail.com>
2017-09-03 22:53:56 +0200

    New awesome rule to detect parameterised internal URLs and resources

---
+++ src/public/default-rules/dom-static-idle-parameterized-href.js
@@ -0,0 +1,79 @@
+function(page, done) {
+  var that = this;
+  var url_s = page.getURL("last");
+  console.log(url_s);
+  var url = new URL(url_s);
+  var maindomain = url.hostname;
+  if (((maindomain.match(/\./g) || []).length) > 2)
+  {
+    maindomain = maindomain.substring(maindomain.indexOf('.'));
+  }
+  var static_dom = page.getStaticDom();
+  var idle_dom = page.getIdleDom();
+  var msg = "";
+  var status = "info";
+
+  var isInternalUrl = (x) =>
+  {
+    var partOfDomain = (u, n) => {
+      try
+      {
+        var turl = new URL(u);
+      }
+      catch(e)
+      {
+        return true;
+      }
+      if (turl.hostname.includes(n))
+      {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+    var href = x.getAttribute('href');
+    //console.log(maindomain);
+    if(!href.includes('//')) { console.log('hey'); console.log(href); return true; }
+    if(href.includes(maindomain)) {
+      return partOfDomain(href,maindomain)
+    }
+    return false;
+  };
+
+  var static_paramstuff  = Array.from(static_dom.querySelectorAll("[href*='?']"));
+  var idle_paramstuff = Array.from(idle_dom.querySelectorAll("[href*='?']"));
+  static_paramstuff = static_paramstuff.filter(isInternalUrl);
+  idle_paramstuff = idle_paramstuff.filter(isInternalUrl);
+  //idle_paramstuff.filter((x) => !((x.href+"").includes('://')));
+  var diff_idle_paramstuff = [];
+  var found = false;
+  for (var ei of idle_paramstuff) {
+    //console.log(ei);
+    found = false;
+    for (var es of static_paramstuff )
+    {
+      if(ei.href === es.href && (ei.outerHTML.toString() === es.outerHTML.toString()))
+      {
+        found = true;
+      }
+    }
+    if(!found)
+    {
+      diff_idle_paramstuff.push(ei);
+    }
+
+  }
+  //console.log('diff');
+  //console.log(diff_idle_paramstuff);
+
+  if (static_paramstuff .length == 0 && idle_paramstuff.length == 0 && diff_idle_paramstuff == 0)
+  {
+    done();
+  }
+  else {
+    //todo: better message
+    msg = static_paramstuff .length+" parameterized internal ressources found in the static DOM "+this.partialCodeLink(static_paramstuff )+", additonal "+diff_idle_paramstuff.length+" parameterized internal ressources found in the idle DOM "+this.partialCodeLink(diff_idle_paramstuff);
+    done(this.createResult('DOM', msg, status));
+  }
+}
