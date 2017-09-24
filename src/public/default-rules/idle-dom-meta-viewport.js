a08222b1f1022eb9a1f2db021cb59ac1aac146c7
Franz Enzenhofer <f.enzenhofer@gmail.com>
2017-09-24 21:39:48 +0200

    rule can now finish

--- src/public/default-rules/idle-dom-meta-viewport.js
+++ src/public/default-rules/idle-dom-meta-viewport.js
@@ -4,5 +4,7 @@ function(page, done) {
   if(meta_viewports && meta_viewports.length > 0)
   {
     done(this.createResult("DOM","<a href='https://developers.google.com/webmasters/mobile-sites/mobile-seo/responsive-design' target='_blank'>Responsive design</a> meta viewport tag ("+meta_viewports.length+") discovered."+this.partialCodeLink(meta_viewports), "info", "Idle"));
+    	return;
   }
+  done();return;
 }
