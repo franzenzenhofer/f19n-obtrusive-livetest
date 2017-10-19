441e19edeedfbc47b6ccea3242e705a53e93c2d2
Moritz Kobrna <github@kobrna.net>
2017-10-19 12:04:20 +0200

    Added dynamic subparts of domains for possible subdomains

--- src/javascripts/components/Popup/Popup.jsx
+++ src/javascripts/components/Popup/Popup.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 
 export default function Popup({ location, disabled, hidden, onToggleSite, onToggleHidden }) {
-  const sites = [
-    [`${location.host}/*`, `${location.protocol}//${location.host}*`],
-    [`*.${location.host.replace(/^www./, '')}/*`, `${location.protocol}//*${location.host.replace(/^www./, '')}*`],
-    ['this page', `${location.href}`],
-  ];
+  let sites = [];
+
+  const withoutTld = location.host.split('.').slice(0, -1);
+  const tld = location.host.split('.').slice(-1).join('.');
+  const subdomainCombinations = withoutTld.map((part, index) => {
+    return [`*.${withoutTld.slice(index).join('.')}.${tld}/*`, `${location.protocol}//*.${withoutTld.slice(index + 1).join('.')}.${tld}*`];
+  });
+
+  sites = sites.concat(
+    [[`${location.host}/*`, `${location.protocol}//${location.host}*`]],
+    subdomainCombinations,
+    [['this page', `${location.href}`]]
+  );
+
   return (
     <div>
-      <p>{disabled ? 'Run tests on' : 'Never run tests on'}</p>
+      <h3>{disabled ? 'Run tests on' : 'Never run tests on'}</h3>
       <ul>
         {sites.map(([text, site]) => <li><a href="#" onClick={() => onToggleSite(site)}>{text}</a></li>)}
       </ul>
-      <button onClick={onToggleHidden}>{hidden ? 'Run in the current tab' : 'Don\'t run in the current tab'}</button>
-      <button onClick={() => window.open(chrome.extension.getURL('rules.html'), '_blank')}>Settings</button>
+      <button className="Button Button--haptic" onClick={onToggleHidden}>{hidden ? 'Run in the current tab' : 'Don\'t run in the current tab'}</button>
+      <button className="Button Button--haptic" onClick={() => window.open(chrome.extension.getURL('rules.html'), '_blank')}>Settings</button>
     </div>
   );
 }
