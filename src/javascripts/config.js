be6b7ed61395f92ae130e220d301412e9fd6e366
Franz Enzenhofer <f.enzenhofer@gmail.com>
2017-10-24 19:13:51 +0200

    new default config

--- src/javascripts/config.js
+++ src/javascripts/config.js
@@ -1,10 +1,14 @@
 export default {
   defaults: {
     sites: `*://*
+!chrome-extension://*
+!chrome-search://*
 !*://*google\\.*
 !*://validator.ampproject.org*
 !*://*webpagetest.org*
-!*://*facebook.com*`,
+!*://*facebook.com*
+!*/wp-admin*
+!*\\.xml`,
     panelPosition: [10, 10],
   },
 };
