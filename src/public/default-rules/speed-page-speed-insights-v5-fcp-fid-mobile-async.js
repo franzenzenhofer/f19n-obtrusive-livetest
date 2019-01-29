f6a9dc0cc65848030a06537224c0c26888634e53
Franz Enzenhofer <f.enzenhofer@gmail.com>
2019-01-29 13:02:06 +0100

    update

--- src/public/default-rules/speed-page-speed-insights-v5-fcp-fid-mobile-async.js
+++ src/public/default-rules/speed-page-speed-insights-v5-fcp-fid-mobile-async.js
@@ -2,7 +2,7 @@ function(page, callback){
   var that = this;
   var key = '%GOOGLEAPIKEY_PSI_NEW%'; //<-- add your API key here https://developers.google.com/speed/docs/insights/v2/first-app#APIKey din't forget to enable it for Google Page Speed Insights
   if(key==='%'+'GOOGLEAPIKEY_PSI_NEW%'){
-    callback(that.createResult('SPEED', '"Page Speed Insights v5 FCP and FID mobile" rule not yet enabled! Set <a href="https://developers.google.com/speed/docs/insights/v5/get-started" target="_blank">Google API Key</a> in <a href="'+that.getGlobals().rulesUrl+'" target="_blank">Settings</a>.', "warning"));
+    callback(that.createResult('SPEED', '"Page Speed Insights v5 Mobile <a href="https://developers.google.com/web/tools/lighthouse/audits/first-contentful-paint" target="_blank"> and <a href="https://developers.google.com/web/updates/2018/05/first-input-delay" target="_blank">FID</a>" rule not yet enabled! Set <a href="https://developers.google.com/speed/docs/insights/v5/get-started" target="_blank">Google API Key</a> in <a href="'+that.getGlobals().rulesUrl+'" target="_blank">Settings</a>.', "warning"));
     return;
   }
   var strategy = 'mobile';
@@ -16,7 +16,7 @@ function(page, callback){
   .then(
     function(response) {
       if (response.status !== 200) {
-        callback(that.createResult('SPEED', 'No Page Speed Insights v5 FCP and FID mobile data. (Response Status '+response.status+' '+response.text+')', "warning"));
+        callback(that.createResult('SPEED', 'No Page Speed Insights v5 Mobile <a href="https://developers.google.com/web/tools/lighthouse/audits/first-contentful-paint" target="_blank"> and <a href="https://developers.google.com/web/updates/2018/05/first-input-delay" target="_blank">FID</a> data. (Response Status '+response.status+' '+response.text+')', "warning"));
         return;
       }
       response.json().then(function(data) {
@@ -27,7 +27,7 @@ function(page, callback){
           var first_contentful_paint_score=data.loadingExperience.metrics.FIRST_CONTENTFUL_PAINT_MS.category;
           var first_input_delay_score=data.loadingExperience.metrics.FIRST_INPUT_DELAY_MS.category;
         } else {
-          callback(that.createResult('SPEED', 'No Page Speed Insights v5 FCP and FID mobile data.', "warning"));
+          callback(that.createResult('SPEED', 'No Page Speed Insights v5 Mobile <a href="https://developers.google.com/web/tools/lighthouse/audits/first-contentful-paint" target="_blank"> and <a href="https://developers.google.com/web/updates/2018/05/first-input-delay" target="_blank">FID</a> data.', "warning"));
           return;
         }
 
@@ -36,6 +36,7 @@ function(page, callback){
         }
         if(first_contentful_paint_data > 2500) {
           fcp_color = "red";
+          type="warning";
         }
 
         if(first_input_delay_data > 50) {
@@ -43,16 +44,17 @@ function(page, callback){
         }
         if(first_input_delay_data > 250) {
           fid_color = "red";
+          type="warning";
         }
 
         callback(that.createResult(
           'SPEED', 
-          'First Contentful Paint (FCP): <span style="background-color:'+fcp_color+';font-weight:bold;">&nbsp;'+first_contentful_paint_data+'&nbsp;</span> <span style="background-color:'+fcp_color+';font-weight:bold;">&nbsp;'+first_contentful_paint_score+'&nbsp;</span><br>First Input Delay (FID): <span style="background-color:'+fid_color+';font-weight:bold;">&nbsp;'+first_input_delay_data+'&nbsp;</span> <span style="background-color:'+fid_color+';font-weight:bold;">&nbsp;'+first_input_delay_score+'&nbsp;</span><br><a href="https://developers.google.com/speed/pagespeed/insights/?hl=en&url='+url+'" target="_blank">Page Speed Insights</a>', type));
+          'First Contentful Paint (<a href="https://developers.google.com/web/tools/lighthouse/audits/first-contentful-paint" target="_blank">FCP</a>): <span style="background-color:'+fcp_color+';font-weight:bold;">&nbsp;'+first_contentful_paint_data+'&nbsp;</span> <span style="background-color:'+fcp_color+';font-weight:bold;">&nbsp;'+first_contentful_paint_score+'&nbsp;</span> - First Input Delay (<a href="https://developers.google.com/web/updates/2018/05/first-input-delay" target="_blank">FID</a>): <span style="background-color:'+fid_color+';font-weight:bold;">&nbsp;'+first_input_delay_data+'&nbsp;</span> <span style="background-color:'+fid_color+';font-weight:bold;">&nbsp;'+first_input_delay_score+'&nbsp;</span> <a href="https://developers.google.com/speed/pagespeed/insights/?hl=en&url='+url+'" target="_blank">Page Speed Insights</a>', type, "Mobile"));
       });
     }
   )
   .catch(function(err) {
-    callback(that.createResult('SPEED', 'No Page Speed Insights v5 FCP and FID <b>mobile</b> data. '+err+' <a href="https://developers.google.com/speed/pagespeed/insights/?hl=en&url='+url+'" target="_blank">Page Speed Insights</a>', "warning"));
+    callback(that.createResult('SPEED', 'No Page Speed Insights v5 Mobile <a href="https://developers.google.com/web/tools/lighthouse/audits/first-contentful-paint" target="_blank"> and <a href="https://developers.google.com/web/updates/2018/05/first-input-delay" target="_blank">FID</a> data. '+err+' <a href="https://developers.google.com/speed/pagespeed/insights/?hl=en&url='+url+'" target="_blank">Page Speed Insights</a>', "warning"));
   });
 
   //callback(this.createResult('test', "async test", "warning"));
--- src/public/default-rules/static-internal-links-check.js
+++ src/public/default-rules/static-internal-links-check.js
@@ -1,6 +1,14 @@
 (page,done) =>
 {
   var do_not_run_string = '%DO_NOT_RUN_DOMAINS%'; //some websites (like stackoverflow) have request limits that block you if you do to many requests, these domains can be added here, comma seperated i.e. stackoverflow.com,stackexchange,ecco-verde
+  var mx_number_of_urls_tested_by_page = '%MAX_NR_OF_URLS_TO_TEST%'; //how many URLs to test per page
+
+  mx_number_of_urls_tested_by_page = parseInt(mx_number_of_urls_tested_by_page);
+  console.log(mx_number_of_urls_tested_by_page);
+  if(mx_number_of_urls_tested_by_page <= 0 || isNaN(mx_number_of_urls_tested_by_page)){mx_number_of_urls_tested_by_page = 15;} 
+  
+  var limit_reached = false;
+  
   let do_not_run = [];
   
 
@@ -119,14 +127,34 @@
     if(!finish)
     {
       let perc = Math.floor((how_many/n.length)*100);
+      if(!limit_reached)
+      {
+        
       msg = msg+"<br>Test stopped after 10sec, "+perc+"% ("+how_many+" of "+n.length+") tested.";
-      if(type==="info"){type="warning";}
+        if(type==="info"){type="warning";}
+      }
+      else
+      {
+
+        msg = msg+"<br>"+mx_number_of_urls_tested_by_page+" random URLs tested, "+perc+"% ("+how_many+" of "+n.length+"). <a href='"+that.getGlobals().rulesUrl+"' target='_blank'>Set URLs limit.</a>";
+      }
+      
     }
     done(that.createResult(lable, msg, type, what));return; 
 	}
 
+  function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+  }
+
 	if(n.length>0)
 	{
+
+    n = shuffle(n);
 
 		for (let ae of n)
   		{
@@ -135,7 +163,7 @@
   			{
   				url_to_fetch = u.origin + url_to_fetch;
   			}
-  			
+
   			that.fetch(url_to_fetch, {
 					responseFormat: 'text',
 					method: 'HEAD'
@@ -166,7 +194,12 @@
            				 	}
            		}
            	linkstested++;
-           	if(linkstested === n.length)
+            if(linkstested === mx_number_of_urls_tested_by_page)
+            {
+              limit_reached = true;
+              endgame(false);  
+            }
+           	else if(linkstested === n.length)
 						{
               is_done = true;
 							endgame();
