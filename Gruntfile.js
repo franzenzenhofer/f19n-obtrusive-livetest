76974e531d1c47def053f010c3580dd9d6889096
neuling <github@kobrna.net>
2019-03-04 13:21:50 +0100

    Fix syntax

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,26 @@
-var sass = require('node-sass');
-var path = require('path');
-var webpack = require('webpack');
+const sass = require('node-sass');
+const path = require('path');
+const webpack = require('webpack');
 
 const webpackConfig = ({ mode, buildDir }) => {
   return {
     mode,
     devtool: false,
     entry: {
-          background: "./src/javascripts/background.js",
-              panel: "./src/javascripts/panel.js",
-              rules: "./src/javascripts/rules.js",
-              popup: "./src/javascripts/popup.js",
-            sandbox: "./src/javascripts/sandbox.js",
-          codeview: "./src/javascripts/codeview.js",
-      content_script: "./src/javascripts/content_script.js",
-        document_end: "./src/javascripts/document_end.js",
-      document_idle: "./src/javascripts/document_idle.js",
-      document_start: "./src/javascripts/document_start.js",
+      background: './src/javascripts/background.js',
+      panel: './src/javascripts/panel.js',
+      rules: './src/javascripts/rules.js',
+      popup: './src/javascripts/popup.js',
+      sandbox: './src/javascripts/sandbox.js',
+      codeview: './src/javascripts/codeview.js',
+      content_script: './src/javascripts/content_script.js',
+      document_end: './src/javascripts/document_end.js',
+      document_idle: './src/javascripts/document_idle.js',
+      document_start: './src/javascripts/document_start.js',
     },
     output: {
       path: path.resolve(__dirname, `./${buildDir}/js`),
-      filename: "[name].js",
+      filename: '[name].js',
     },
     resolve: {
       modules: [
@@ -29,15 +29,16 @@ const webpackConfig = ({ mode, buildDir }) => {
         'javascripts/utils',
         'node_modules',
       ],
-      extensions: [
-        '.js',
-        '.jsx',
-      ],
+      extensions: ['.js', '.jsx'],
     },
     module: {
       rules: [
-        { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader?presets[]=es2015,presets[]=react,presets[]=stage-0" }
-      ]
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader?presets[]=es2015,presets[]=react,presets[]=stage-0',
+        },
+      ],
     },
     plugins: [
       new webpack.DefinePlugin({
@@ -48,27 +49,27 @@ const webpackConfig = ({ mode, buildDir }) => {
   };
 };
 
-module.exports = function(grunt) {
+module.exports = (grunt) => {
   grunt.initConfig({
-    pkg: grunt.file.readJSON("package.json"),
+    pkg: grunt.file.readJSON('package.json'),
 
     config: {
       dev: {
         options: {
           variables: {
-            webpackMode: "development",
-            buildDir: "build",
-          }
-        }
+            webpackMode: 'development',
+            buildDir: 'build',
+          },
+        },
       },
       dist: {
         options: {
           variables: {
-            webpackMode: "production",
-            buildDir: "dist",
-          }
-        }
-      }
+            webpackMode: 'production',
+            buildDir: 'dist',
+          },
+        },
+      },
     },
 
     clean: ["<%= grunt.config.get('buildDir') %>/**/*"],
@@ -78,75 +79,83 @@ module.exports = function(grunt) {
         files: [
           {
             expand: true,
-            src: "**",
+            src: '**',
             dest: "<%= grunt.config.get('buildDir') %>/",
-            cwd: "src/public",
-            dot: true
-          }
-        ]
-      }
+            cwd: 'src/public',
+            dot: true,
+          },
+        ],
+      },
     },
 
     sass: {
       dev: {
         options: {
           implementation: sass,
-          style: "expanded",
-          includePaths: [].concat(require('node-bourbon').includePaths).concat(require('node-neat').includePaths),
+          style: 'expanded',
+          includePaths: []
+            .concat(require('node-bourbon').includePaths)
+            .concat(require('node-neat').includePaths),
           noCache: true,
-          sourceMap: false
+          sourceMap: false,
         },
         files: {
-          "<%= grunt.config.get('buildDir') %>/css/application.css": "src/stylesheets/application.sass",
-          "<%= grunt.config.get('buildDir') %>/css/panel.css": "src/stylesheets/panel.sass",
-          "<%= grunt.config.get('buildDir') %>/css/rules.css": "src/stylesheets/rules.sass",
-          "<%= grunt.config.get('buildDir') %>/css/popup.css": "src/stylesheets/popup.sass",
-          "<%= grunt.config.get('buildDir') %>/css/content_styles.css": "src/stylesheets/content_styles.sass"
-        }
-      }
+          "<%= grunt.config.get('buildDir') %>/css/application.css":
+            'src/stylesheets/application.sass',
+          "<%= grunt.config.get('buildDir') %>/css/panel.css":
+            'src/stylesheets/panel.sass',
+          "<%= grunt.config.get('buildDir') %>/css/rules.css":
+            'src/stylesheets/rules.sass',
+          "<%= grunt.config.get('buildDir') %>/css/popup.css":
+            'src/stylesheets/popup.sass',
+          "<%= grunt.config.get('buildDir') %>/css/content_styles.css":
+            'src/stylesheets/content_styles.sass'
+        },
+      },
     },
 
     webpack: {
-      someName: () => webpackConfig({
-        mode: grunt.config.get('webpackMode'),
-        buildDir: grunt.config.get('buildDir'),
-      }),
+      someName: () =>
+        webpackConfig({
+          mode: grunt.config.get('webpackMode'),
+          buildDir: grunt.config.get('buildDir'),
+        }),
     },
 
     zip: {
-      "using-cwd": {
+      'using-cwd': {
         cwd: "<%= grunt.config.get('buildDir') %>/",
         src: "<%= grunt.config.get('buildDir') %>/**/*",
-        dest: "<%= grunt.config.get('buildDir') %>/export.zip"
-      }
+        dest: "<%= grunt.config.get('buildDir') %>/export.zip",
+      },
     },
 
     watch: {
       scripts: {
-        files: ["src/**"],
-        tasks: ["default"],
+        files: ['src/**'],
+        tasks: ['default'],
         options: {
           spawn: false,
-          livereload: true
-        }
-      }
-    }
+          livereload: true,
+        },
+      },
+    },
   });
 
-  grunt.loadNpmTasks("grunt-sass");
-  grunt.loadNpmTasks("grunt-config");
-  grunt.loadNpmTasks("grunt-webpack");
-  grunt.loadNpmTasks("grunt-contrib-watch");
-  grunt.loadNpmTasks("grunt-contrib-concat");
-  grunt.loadNpmTasks("grunt-contrib-copy");
-  grunt.loadNpmTasks("grunt-contrib-clean");
-  grunt.loadNpmTasks("grunt-notify");
-  grunt.loadNpmTasks("grunt-zip");
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-config');
+  grunt.loadNpmTasks('grunt-webpack');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-notify');
+  grunt.loadNpmTasks('grunt-zip');
 
-  grunt.registerTask("main", ["clean", "webpack", "sass", "copy"]);
+  grunt.registerTask('main', ['clean', 'webpack', 'sass', 'copy']);
 
-  var defaultTasks = ["config:dev", "main"];
+  const defaultTasks = ['config:dev', 'main'];
 
-  grunt.registerTask("default", defaultTasks.concat(["watch"]));
-  grunt.registerTask("dist", ["config:dist", "main", "zip"]);
+  grunt.registerTask('default', defaultTasks.concat(['watch']));
+  grunt.registerTask('dist', ['config:dist', 'main', 'zip']);
 };
