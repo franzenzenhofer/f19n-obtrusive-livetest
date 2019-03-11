9209fa06d933e26b5dca1260643af4f07d7288e6
neuling <github@kobrna.net>
2019-03-11 14:06:56 +0100

    Retry live reload connection every 5 seconds if failed

--- src/javascripts/utils/extensionLiveReload.js
+++ src/javascripts/utils/extensionLiveReload.js
@@ -1,6 +1,6 @@
 const setup = (host = 'localhost', port = 35729) => {
   const connection = new WebSocket(`ws://${host}:${port}/livereload`);
-  connection.onerror = (error) => { console.log('reload connection got error:', error); };
+  connection.onerror = (error) => { console.log('Live reload connection error. Retry in 5 seconds …'); setTimeout(() => { setup(host, port); }, 5000); };
   connection.onmessage = () => { chrome.runtime.reload(); };
 };
 
