ac37f05303c73906a3651bebf650f50e26f1cf38
Franz Enzenhofer <f.enzenhofer@gmail.com>
2019-03-12 12:57:20 +0100

    new page object debugging

--- src/public/default-rules/debug-stringify-page-object.js
+++ src/public/default-rules/debug-stringify-page-object.js
@@ -2,7 +2,7 @@ function(page,done){
 	var debug = '%DEBUG%'; //must be set to true
   	if(debug==='true')
   	{
-		done(this.createResult("DEBUG", "Complete Page Object "+this.stringifyLink(page), "info", null, 9000));
+		done(this.createResult("DEBUG", "Complete Page Object "+this.stringifyLink(page), "info", 9000));
 	}
 	done();
 	return null;
